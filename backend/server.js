b882e9c67e0db0882dcf6cafa6434f12cb9dc95d
trox115 <tofernandes67@gmail.com>
2020-06-26 01:08:45 +0100

    add schema, models and requests

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,12 @@ connection.once("open", () => {
   );
 });
 
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
 app.listen(port, () => {
   console.log(
     `Server is running on port: ${port}`
